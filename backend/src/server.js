09427ac0b7043b7d37b0c64792ded43602f61df0
MICA- The School of Ideas <oem@micamail.in>
2025-07-16 16:04:58 +0530

    Update server.js
    updated port statement

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,6 +17,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 // Connect to database
 connectDB();
